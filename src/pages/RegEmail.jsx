85d007be8f131bb5f904a215eeb6cc63b5737df9
Murilo <muriloflimaa@gmail.com>
2022-07-01 16:23:08 -0300

    modal entrar sem funcao

--- src/pages/RegEmail.jsx
+++ src/pages/RegEmail.jsx
@@ -6,62 +6,62 @@ import { useRouter } from 'next/router'
 import { useLocalStorage } from '../utils/useLocalStorage'
 
 export default function RegEmail() {
-    const [email] = useLocalStorage('email', '')
-    const router = useRouter()
-    const formik = useFormik({
-        initialValues: {
-            email: email,
-        },
-        validationSchema: yup.object({
-            email: yup
-                .string()
-                .email('Digite um e-mail válido!')
-                .required('O campo é obrigatório.'),
-        }),
-        onSubmit: (values) => {
-            localStorage.setItem('email', JSON.stringify(values.email))
-            router.push('/RegCelular')
-        },
-    })
+  const [email] = useLocalStorage('email', '')
+  const router = useRouter()
+  const formik = useFormik({
+    initialValues: {
+      email: email,
+    },
+    validationSchema: yup.object({
+      email: yup
+        .string()
+        .email('Digite um e-mail válido!')
+        .required('O campo é obrigatório.'),
+    }),
+    onSubmit: (values) => {
+      localStorage.setItem('email', JSON.stringify(values.email))
+      router.push('/RegCelular')
+    },
+  })
 
-    return (
-        <>
-            <form onSubmit={formik.handleSubmit}>
-                <h1 className="pb-5 text-black text-xl font-semibold">
-                    Qual seu email?
-                </h1>
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <h1 className="pb-5 text-black text-xl font-semibold">
+          Qual seu email?
+        </h1>
 
-                <TextField
-                    label="Seu Email: (Ex: BuyPhone@gmail.com)"
-                    type="text"
-                    name="email"
-                    fullWidth
-                    variant="outlined"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.email}
-                />
+        <TextField
+          label="Seu Email"
+          type="text"
+          name="email"
+          fullWidth
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
 
-                {formik.touched.email && formik.errors.email ? (
-                    <div className="badge badge-warning badge-lg bg-opacity-80 text-xs mt-[1.5rem]">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="stroke-current flex-shrink-0 h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                            />
-                        </svg>
-                        <span className="pl-2"> {formik.errors.email}</span>
-                    </div>
-                ) : null}
-                <Button />
-            </form>
-        </>
-    )
+        {formik.touched.email && formik.errors.email ? (
+          <div className="badge badge-warning badge-lg bg-opacity-80 text-xs mt-[1.5rem]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span className="pl-2"> {formik.errors.email}</span>
+          </div>
+        ) : null}
+        <Button />
+      </form>
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,74 +3,124 @@ import Link from 'next/link'
 import styles from './styles.module.scss'
 import Image from 'next/image'
 import LogoSvg from '../../public/LogoPurple.svg'
+import { TextField } from '@mui/material'
+import Button from './components/Button'
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>BuyPhone</title>
-                <meta
-                    name="description"
-                    content="Buy Phone para revendedores!"
-                />
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>BuyPhone</title>
+        <meta name="description" content="Buy Phone para revendedores!" />
+      </Head>
 
-            <div className="min-h-screen w-full relative bg-white flex items-center justify-center">
-                <div className="glass z-10 absolute left-0 w-full h-full"></div>
-                <ul className={styles.circles}>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+      <div className="min-h-screen w-full relative bg-white flex items-center justify-center">
+        <div className="glass z-10 absolute left-0 w-full h-full"></div>
+        <ul className={styles.circles}>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
 
-                <div className="flex flex-col items-center lg:flex-row">
-                    <div className="flex flex-col">
-                        <div className="pt-4 w-3/5 h-auto mx-auto z-20 relative pb-2 lg:w-80">
-                            <Image
-                                src={LogoSvg}
-                                layout="responsive"
-                                quality={100}
-                                alt="Logo BuyPhone"
-                            />
-                        </div>
-                        <div className="flex flex-col items-center lg:flex-row">
-                            <div className="flex flex-col items-center pt-4 z-20 relative">
-                                <img src="/Cel.png" alt="Celular Flutuando" />
-                            </div>
-                            <div className="card max-w-lg mx-auto h-1/4 text-Loja shadow-lg lg:bg-white z-20">
-                                <div className="card-body ">
-                                    <h1 className="font-bold text-2xl">
-                                        A oportunidade de aumentar o lucro de
-                                        suas vendas
-                                    </h1>
-                                    <p className="text-xs">
-                                        Tenha acesso a preços imbatíveis de
-                                        iPhones nacionais lacrados para revenda!
-                                    </p>
-                                    <a
+        <div className="flex flex-col items-center lg:flex-row">
+          <div className="flex flex-col">
+            <div className="pt-4 w-3/5 h-auto mx-auto z-20 relative pb-2 lg:w-80">
+              <Image
+                src={LogoSvg}
+                layout="responsive"
+                quality={100}
+                alt="Logo BuyPhone"
+              />
+            </div>
+            <div className="flex flex-col items-center lg:flex-row">
+              <div className="flex flex-col items-center pt-4 z-20 relative">
+                <img src="/Cel.png" alt="Celular Flutuando" />
+              </div>
+              <div className="card max-w-lg mx-auto h-1/4 text-Loja shadow-lg lg:bg-white z-20">
+                <div className="card-body ">
+                  <h1 className="font-bold text-2xl">
+                    A oportunidade de aumentar o lucro de suas vendas
+                  </h1>
+                  <p className="text-xs">
+                    Tenha acesso a preços imbatíveis de iPhones nacionais
+                    lacrados para revenda!
+                  </p>
+                  {/* <a
                                         className="w-full h-[38px] text-white font-medium text-xs uppercase bg-Loja rounded-md flex items-center justify-center hover:scale-105 ease-in-out duration-300"
-                                        href="#"
+                                        id="my-modal-6"
                                     >
                                         Entrar
-                                    </a>
-                                    <Link href={'/Welcome'}>
-                                        <a className="w-full h-[38px] border font-medium text-xs uppercase border-Loja rounded-md flex items-center justify-center hover:scale-105 ease-in-out duration-300">
-                                            Cadastrar
-                                        </a>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                                    </a> */}
+
+                  <label
+                    htmlFor="my-modal-4"
+                    className="w-full h-[38px] text-white font-medium text-xs uppercase bg-Loja rounded-md flex items-center justify-center hover:scale-105 ease-in-out duration-300"
+                  >
+                    entrar
+                  </label>
+
+                  <input
+                    type="checkbox"
+                    id="my-modal-4"
+                    className="modal-toggle"
+                  />
+                  <label
+                    htmlFor="my-modal-4"
+                    className="modal cursor-pointer modal-bottom md:bg-white sm:modal-middle"
+                  >
+                    <label
+                      className="modal-box relative gap-3 h-[70vh] justify-between pb-14 p-4 flex flex-col text-center items-center bg-white shadow-lg"
+                      htmlFor=""
+                    >
+                      <div className="h-[1px] bg-Loja mt-[0.85rem] opacity-50 w-5/6 absolute"></div>
+                      <h1 className="text-lg font-bold z-10 flex justify-center uppercase w-24 bg-white">
+                        Entrar
+                      </h1>
+                      <TextField
+                        label="Email"
+                        type="text"
+                        name="email"
+                        fullWidth
+                        variant="outlined"
+                      />
+                      <TextField
+                        label="Senha"
+                        type="password"
+                        name="email"
+                        fullWidth
+                        variant="outlined"
+                      />
+                      <button
+                        className="flex mt-[1.5rem] p-2 justify-center mx-auto font-medium uppercase w-full text-white text-xl py-3 rounded-md hover:scale-105 ease-in-out duration-300 bg-Loja"
+                        type="submit"
+                      >
+                        Entrar
+                      </button>
+
+                      <a className="font-semibold" href="">
+                        Esqueci minha senha{' '}
+                      </a>
+                    </label>
+                  </label>
+
+                  <Link href={'/Welcome'}>
+                    <a className="w-full h-[38px] border font-medium text-xs uppercase border-Loja rounded-md flex items-center justify-center hover:scale-105 ease-in-out duration-300">
+                      Cadastrar
+                    </a>
+                  </Link>
                 </div>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
