b68ea827ff40c8f50f4e56f451e179060862282f
Murilo <muriloflimaa@gmail.com>
2022-07-13 15:06:57 -0300

    correcoes finais LP

--- src/pages/components/checkbox/index.tsx
+++ src/pages/components/checkbox/index.tsx
@@ -37,8 +37,9 @@ interface Check {
 
 export default function Checkbox({ value, value2 }: Check) {
   return (
-    <div className="flex w-max">
+    <div className="flex w-max relative">
       <input
+        className="sr-only peer"
         type="checkbox"
         value="yes"
         name="answer"
@@ -46,14 +47,13 @@ export default function Checkbox({ value, value2 }: Check) {
         placeholder="Vanilla"
       />
 
+      <CheckIcon className="hidden absolute ml-3 w-4 h-4 text-white peer-checked:flex mt-[0.5rem]" />
       <label
         className="flex p-2 bg-white border border-gray-300 rounded-2xl w-full cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:bg-Loja peer-checked:text-white peer-checked:border-transparent"
         htmlFor={value2}
       >
         {value}
       </label>
-
-      <CheckIcon className="hidden w-5 h-5 text-white peer-checked:flex -ml-10 mt-3" />
     </div>
   )
 }
