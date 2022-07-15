c0f289b5db75999a5b0eb4981fc0604fbcb9a353
Murilo <muriloflimaa@gmail.com>
2022-07-15 15:37:21 -0300

    correcao icon/page category

--- src/pages/components/checkbox/index.tsx
+++ src/pages/components/checkbox/index.tsx
@@ -17,9 +17,9 @@ export default function Checkbox({ value, value2 }: Check) {
         placeholder="Vanilla"
       />
 
-      <CheckIcon className="hidden absolute ml-3 w-4 h-4 text-white peer-checked:flex mt-[0.3rem]" />
+      <CheckIcon className="hidden absolute ml-3 w-4 h-4 text-white peer-checked:z-20 peer-checked:flex mt-[0.3rem]" />
       <label
-        className="flex px-3 py-1 bg-white border transition delay-[70ms] border-gray-300 rounded-2xl text-2xs w-full cursor-pointer peer-checked:pl-[2.2rem] focus:outline-none hover:bg-gray-50 peer-checked:bg-Loja peer-checked:text-white peer-checked:border-transparent"
+        className="flex px-3 py-1 bg-white border z-10  transition delay-[70ms] border-gray-300 rounded-2xl text-2xs w-full cursor-pointer peer-checked:pl-[2.2rem] focus:outline-none hover:bg-gray-50 peer-checked:bg-Loja peer-checked:text-white peer-checked:border-transparent"
         htmlFor={value2}
       >
         {value}
